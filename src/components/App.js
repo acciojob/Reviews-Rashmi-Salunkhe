860ada321883bb2b702316a882bdfaeae0e76653
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-24 12:19:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,38 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import Review from "./Review";
+import data from "../data.js";
+import "../styles/App.css";
 
+function App() {
+  const [index, setIndex] = useState(0);
+
+  const nextReview = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % data.length);
+  };
+
+  const prevReview = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length);
+  };
+
+  const randomReview = () => {
+    let newIndex;
+    do {
+      newIndex = Math.floor(Math.random() * data.length);
+    } while (newIndex === index);
+    setIndex(newIndex);
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h1 id="review-heading">Our Reviews</h1>
+        <Review review={data[index]} nextReview={nextReview} prevReview={prevReview} randomReview={randomReview}/>
+        <div className="button-container">
+  
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
